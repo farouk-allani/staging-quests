22d736fad5a0b76b599367bb341c85f2815825b1
farouk-allani <farouk-allani@users.noreply.github.com>
2025-09-23 11:28:06 +0100

    remve points in recent activity since it is wrong and not coming from backend

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -377,11 +377,11 @@ export default function ProgressPage() {
                         </div>
                       </div>
                       
-                      {submission.points && (
+                      {/* {submission.points && (
                         <div className="text-xs sm:text-sm font-medium font-mono bg-gradient-to-r from-green-500 to-emerald-500 bg-clip-text text-transparent flex-shrink-0">
                           +{submission.points} PTS
                         </div>
-                      )}
+                      )} */}
                     </div>
                   ))}
                 </div>
